3925b82a95c086dda2698d1f4757e42b3d04512d
ken5scal <kengoscal@gmail.com>
2017-06-30 13:40:30 +0900

    Change layout to List View

--- app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
+++ app/src/main/kotlin/sample/qiitaclient/MainActivity.kt
@@ -1,26 +1,30 @@
 package sample.qiitaclient
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.ListView
 import sample.qiitaclient.model.Article
 import sample.qiitaclient.model.User
-import sample.qiitaclient.view.ArticleView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-        var articleView = ArticleView(applicationContext)
-        articleView.setArticle(
-                Article(
-                        id = "124",
-                        title = "Intro to Kotlin",
-                        url = "",
-                        user = User(id = "123", name = "kengo", profileImageUrl = "")
-                )
-        )
-        setContentView(articleView)
+        setContentView(R.layout.activity_main)
+
+        val listAdapter = ArticleListAdapter(applicationContext)
+        listAdapter.articles = listOf(
+                dummyArticle("Kotlin", "Kengo"),
+                dummyArticle("Kotlin", "Kengo"))
+
+        val listView: ListView = findViewById(R.id.list_view) as ListView
+        listView.adapter = listAdapter
     }
+
+    private fun dummyArticle(title: String, userName: String): Article =
+            Article(id = "",
+                    title = title,
+                    url = "https://kotlinlang.org/",
+                    user = User(id = "", name = userName, profileImageUrl = ""))
 
 }

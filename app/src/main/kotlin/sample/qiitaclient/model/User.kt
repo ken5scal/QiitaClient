4e69e1305d3ee03a33417009f9a1cde4475dc7e3
ken5scal <kengoscal@gmail.com>
2017-06-30 12:14:21 +0900

    Defined models

---
+++ app/src/main/kotlin/sample/qiitaclient/model/User.kt
@@ -0,0 +1,8 @@
+package sample.qiitaclient.model
+
+/**
+ * Created by suzuki on 2017/06/30.
+ */
+data class User(val id: String,
+                val name: String,
+                val profileImageUrl: String)

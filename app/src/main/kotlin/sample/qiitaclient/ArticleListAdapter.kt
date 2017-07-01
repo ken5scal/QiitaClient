564d2753c412a5944b66539cf2a432bb6496aec4
ken5scal <kengoscal@gmail.com>
2017-07-01 22:08:38 +0900

    refactored a bit

--- app/src/main/kotlin/sample/qiitaclient/ArticleListAdapter.kt
+++ app/src/main/kotlin/sample/qiitaclient/ArticleListAdapter.kt
@@ -9,12 +9,9 @@ import sample.qiitaclient.view.ArticleView
 
 class ArticleListAdapter(private val context: Context) : BaseAdapter() {
     var articles: List<Article> = emptyList()
-    override fun getCount(): Int = articles.size
-    //    override fun getCount(): Int {
-//        return articles.size
-//    }
-    override fun getItem(position: Int): Any? = articles[position]
 
+    override fun getCount(): Int = articles.size
+    override fun getItem(position: Int): Any? = articles[position]
     override fun getItemId(position: Int): Long = 0
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View =
             ((convertView as? ArticleView) ?: ArticleView(context)).apply {

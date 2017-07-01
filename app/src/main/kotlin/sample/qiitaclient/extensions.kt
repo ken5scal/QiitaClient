9c3d95f574cb7a37adfea867050da371c28d0db8
ken5scal <kengoscal@gmail.com>
2017-07-01 21:59:20 +0900

    Change Article View to Databinding

--- app/src/main/kotlin/sample/qiitaclient/extensions.kt
+++ app/src/main/kotlin/sample/qiitaclient/extensions.kt
@@ -1,17 +1,22 @@
 package sample.qiitaclient
 
 import android.content.Context
+import android.databinding.BindingAdapter
 import android.support.annotation.IdRes
 import android.view.View
+import android.widget.ImageView
 import android.widget.Toast
+import com.bumptech.glide.Glide
 
-/**
- * Created by suzuki on 2017/06/30.
- */
 fun <T : View> View.bindView(@IdRes id: Int): Lazy<T> = lazy {
     findViewById(id) as T
 }
 
 fun Context.toast(message: String, duration: Int = Toast.LENGTH_SHORT) {
     Toast.makeText(this, message, duration).show()
+}
+
+@BindingAdapter("bind:imageUrl")
+fun ImageView.loadImage(url: String) {
+    Glide.with(context).load(url).into(this)
 }
--- app/src/main/kotlin/sample/qiitaclient/view/ArticleView.kt
+++ app/src/main/kotlin/sample/qiitaclient/view/ArticleView.kt
@@ -1,15 +1,12 @@
 package sample.qiitaclient.view
 
 import android.content.Context
-import android.graphics.Color
+import android.databinding.DataBindingUtil
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.FrameLayout
-import android.widget.ImageView
-import android.widget.TextView
-import com.bumptech.glide.Glide
 import sample.qiitaclient.R
-import sample.qiitaclient.bindView
+import sample.qiitaclient.databinding.ViewArticleBinding
 import sample.qiitaclient.model.Article
 
 class ArticleView : FrameLayout {
@@ -21,18 +18,14 @@ class ArticleView : FrameLayout {
                 defStyleAttr: Int,
                 defstyleRes: Int) : super(context, attrs, defStyleAttr, defstyleRes)
 
-    val profileImageView: ImageView by bindView(R.id.profile_image_view)
-    val titleTextView: TextView by bindView(R.id.title_text_view)
-    val userNameTextView: TextView by bindView(R.id.user_name_text_view)
+    val binding: ViewArticleBinding
 
     init {
-        LayoutInflater.from(context).inflate(R.layout.view_article, this)
+        binding = DataBindingUtil.inflate(LayoutInflater.from(context), R.layout.view_article, this, true)
     }
 
     fun setArticle(article: Article) {
-        titleTextView.text = article.title
-        userNameTextView.text = article.user.name
-        Glide.with(context).load(article.user.profileImageUrl).into(profileImageView)
+        binding.article = article
     }
 
 }
